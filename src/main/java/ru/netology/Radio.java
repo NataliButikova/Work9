d8f8763228eb5779883db9976897a05ed9ad96cf
Natali Butikova <Butikova87@bk.ru>
2024-06-16 20:18:42 +0300

    new branch

--- src/main/java/ru/netology/Radio.java
+++ src/main/java/ru/netology/Radio.java
@@ -1,63 +1,76 @@
 package ru.netology;
 
 public class Radio {
-    private int currentStation;
-    private int currentVolume;
+
+    private int maxStation = 9; private int minStation = 0;
+    private int currentStation = minStation;
+    private int maxVolume = 100;
+    private int minVolume = 0;
+    private int currentVolume = minVolume;
+
+    public Radio() {
+        this.minVolume = minVolume;
+        this.maxVolume = maxVolume;
+        this.currentVolume = minVolume;
+    }
+
+    public Radio(int size) {
+        maxStation = minStation + size - 1;
+    }
 
     public int getCurrentStation() {
         return currentStation;
-    }
-
-    public void setCurrentStation(int currentStation) {
-        if (currentStation < 0) {
-            return;
-        }
-        if (currentStation > 9) {
-            return;
-        }
-        this.currentStation = currentStation;
     }
 
     public int getCurrentVolume() {
         return currentVolume;
     }
 
-    public void setCurrentVolume(int currentVolume) {
-        if (currentVolume < 0) {
+    public int getMaxStation() {
+        return maxStation;
+    }
+
+    public int getMinStation() {
+        return minStation;
+    }
+
+    public void setCurrentStation(int newCurrentStation) {
+
+        if (newCurrentStation > maxStation) {
             return;
         }
-        if (currentVolume > 100) {
+        currentStation = newCurrentStation;
+    }
+
+    public void NextStation() {
+        if (currentStation < maxStation) {
+            currentStation = currentStation + 1;
+        } else currentStation = minStation;
+    }
+
+    public void PrevStation() {
+        if (currentStation > minStation) {
+            currentStation = currentStation - 1;
+        } else currentStation = maxStation;
+    }
+
+    public void setCurrentVolume(int newCurrentVolume) {
+
+        if (newCurrentVolume > 100) {
             return;
         }
-        this.currentVolume = currentVolume;
+        currentVolume = newCurrentVolume;
     }
 
-    public void nextStation() {
-        if (currentStation != 9) {
-            currentStation++;
-        } else {
-            currentStation = 0;
-        }
-    }
-
-    public void prevStation() {
-        if (currentStation != 0) {
-            currentStation--;
-        } else {
-            currentStation = 9;
-        }
-    }
-
-    public void nextVolume() {
+    public void NextVolume() {
         if (currentVolume < 100) {
             currentVolume = currentVolume + 1;
-        }
+        } else currentVolume = 0;
     }
 
-    public void prevVolume() {
+    public void PrevVolume() {
         if (currentVolume > 0) {
             currentVolume = currentVolume - 1;
         }
     }
 }
-
